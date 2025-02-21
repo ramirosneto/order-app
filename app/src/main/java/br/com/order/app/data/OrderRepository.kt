c682d2ac14f082cb15902054dc117073e9658bce
ramiro.neto.ext <ramiro.neto.ext@panonline.com.br>
2025-02-20 21:26:12 -0300

    create new methods

--- app/src/main/java/br/com/order/app/data/OrderRepository.kt
+++ app/src/main/java/br/com/order/app/data/OrderRepository.kt
@@ -1,16 +1,39 @@
 package br.com.order.app.data
 
-class OrderRepository constructor(private val orderDao: OrderDao) {
+import br.com.order.app.model.Order
+import br.com.order.app.model.OrderItem
+import br.com.order.app.utils.OrderMapper
+import java.time.LocalDateTime
 
-    suspend fun insertOrder(order: Order): Long {
-        return orderDao.insertOrder(order)
-    }
+class OrderRepository(
+    private val mapper: OrderMapper,
+    private val orderDao: OrderDao
+) {
 
-    suspend fun insertOrderItem(orderItem: OrderItem) {
-        orderDao.insertOrderItem(orderItem)
+    suspend fun insertOrder(orderItems: List<OrderItem>) {
+        val totalAmount = orderItems.sumOf { it.totalPrice }
+        val orderEntity = OrderEntity(date = LocalDateTime.now(), totalAmount = totalAmount)
+        val orderId = orderDao.insertOrder(orderEntity)
+        orderItems.forEach { orderItem ->
+            orderDao.insertOrderItem(mapper.mapOrderItemModelToEntity(orderId, orderItem))
+        }
     }
 
     suspend fun getAllOrders(): List<Order> {
-        return orderDao.getAllOrders()
+        val orders = arrayListOf<Order>()
+        orderDao.getAllOrders().forEach { order ->
+            orders.add(mapper.mapOrderEntityToModel(order))
+        }
+
+        return orders
+    }
+
+    suspend fun getAllOrderItems(orderId: Int): List<OrderItem> {
+        val orderItems = arrayListOf<OrderItem>()
+        orderDao.getAllOrderItems(orderId).forEach { orderItem ->
+            orderItems.add(mapper.mapOrderItemEntityToModel(orderItem))
+        }
+
+        return orderItems
     }
 }

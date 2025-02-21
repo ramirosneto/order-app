7abadfd5616b47a40d1eab904c5fc6176ca63505
ramiro.neto.ext <ramiro.neto.ext@panonline.com.br>
2025-02-21 19:25:58 -0300

    adjusting bottomsheet

--- app/src/main/java/br/com/order/app/utils/Extensions.kt
+++ app/src/main/java/br/com/order/app/utils/Extensions.kt
@@ -1,21 +1,21 @@
 package br.com.order.app.utils
 
+import android.annotation.SuppressLint
 import java.text.NumberFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-fun String.toLocalDateTime(): LocalDateTime {
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-    return LocalDateTime.parse(this, formatter)
-}
 
-fun LocalDateTime.format(): String {
-    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")
-    return this.format(formatter)
+@SuppressLint("SimpleDateFormat")
+fun String.formatDate(): String {
+    val parser = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+    val formatter = SimpleDateFormat("dd/MM/yyyy HH:mm")
+    return formatter.format(parser.parse(this)!!)
 }
 
 fun Double.formatPrice(): String {
-    val numberFormat = NumberFormat.getCurrencyInstance()
+    val brLocale = Locale("pt", "BR")
+    val numberFormat = NumberFormat.getCurrencyInstance(brLocale)
     numberFormat.maximumFractionDigits = 2
     return numberFormat.format(this)
 }

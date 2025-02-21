1b269d592b7b3e015dd16559bb4d1f5f03fb53a0
ramiro.neto.ext <ramiro.neto.ext@panonline.com.br>
2025-02-20 21:25:14 -0300

    create mapper to repository

---
+++ app/src/main/java/br/com/order/app/utils/OrderMapper.kt
@@ -0,0 +1,44 @@
+package br.com.order.app.utils
+
+import br.com.order.app.data.OrderEntity
+import br.com.order.app.data.OrderItemEntity
+import br.com.order.app.model.Order
+import br.com.order.app.model.OrderItem
+
+class OrderMapper {
+
+    fun mapOrderModelToEntity(orderModel: Order): OrderEntity {
+        return OrderEntity(
+            orderId = orderModel.orderId,
+            date = orderModel.date,
+            totalAmount = orderModel.totalAmount
+        )
+    }
+
+    fun mapOrderEntityToModel(orderEntity: OrderEntity): Order {
+        return Order(
+            orderId = orderEntity.orderId,
+            date = orderEntity.date,
+            totalAmount = orderEntity.totalAmount
+        )
+    }
+
+    fun mapOrderItemModelToEntity(orderId: Int, orderItemModel: OrderItem): OrderItemEntity {
+        return OrderItemEntity(
+            orderId = orderId,
+            description = orderItemModel.description,
+            quantity = orderItemModel.quantity,
+            unitPrice = orderItemModel.unitPrice,
+            totalPrice = orderItemModel.totalPrice
+        )
+    }
+
+    fun mapOrderItemEntityToModel(orderItemEntity: OrderItemEntity): OrderItem {
+        return OrderItem(
+            description = orderItemEntity.description,
+            quantity = orderItemEntity.quantity,
+            unitPrice = orderItemEntity.unitPrice,
+            totalPrice = orderItemEntity.totalPrice
+        )
+    }
+}

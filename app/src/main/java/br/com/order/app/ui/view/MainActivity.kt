66e92f903c408a1f755f7078ef2374af5e8f2133
ramiro.neto.ext <ramiro.neto.ext@panonline.com.br>
2025-02-21 00:31:32 -0300

    adjusting viewmodel

--- app/src/main/java/br/com/order/app/ui/view/MainActivity.kt
+++ app/src/main/java/br/com/order/app/ui/view/MainActivity.kt
@@ -3,20 +3,20 @@ package br.com.order.app.ui.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import br.com.order.app.ui.main.MainScreen
 import br.com.order.app.ui.theme.OrderAppTheme
 import br.com.order.app.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             OrderAppTheme {
-                MainScreen(viewModel)
+                MainScreen(mainViewModel)
             }
         }
     }
--- app/src/main/java/br/com/order/app/viewmodel/MainViewModel.kt
+++ app/src/main/java/br/com/order/app/viewmodel/MainViewModel.kt
@@ -3,12 +3,26 @@ package br.com.order.app.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.order.app.data.OrderRepository
+import br.com.order.app.model.Order
 import br.com.order.app.model.OrderItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val repository: OrderRepository) : ViewModel() {
 
-    //private val _orders: MutableState<List<Order>> = mutableStateListOf<Order>()
+    private val _orders = MutableStateFlow<List<Order>?>(null)
+    val orders: StateFlow<List<Order>?> get() = _orders
+
+    init {
+        getOrders()
+    }
+
+    private fun getOrders() {
+        viewModelScope.launch {
+            _orders.value = repository.getAllOrders()
+        }
+    }
 
     fun addOrder(orderItems: List<OrderItem>) {
         viewModelScope.launch {
